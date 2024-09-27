f0a786aa9367480d941f213cb0def48ed663ec18
PaulBredenberg <paulbredenberg@gmail.com>
2024-09-27 13:12:47 -0400

    test: add vue test case

---
+++ test-cases/vue/ExampleComponent.vue
@@ -0,0 +1,15 @@
+<template>
+   <div>
+      <h1>Hello World</h1>
+   </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+
+let myVar = null;
+
+onMounted(async () => {
+   myVar = myVar === null ?? 'test';
+});
+</script>
