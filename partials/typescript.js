4290229dddc4f449d472ebe5e7db826414c61f41
Matheus Baldi <matheushbaldi@gmail.com>
2024-12-26 13:22:34 -0300

    fix: enable @typescript-eslint/space-infix-ops

--- partials/typescript.js
+++ partials/typescript.js
@@ -213,5 +213,10 @@ module.exports = {
             'typedefs': false,
          },
       ],
+
+      // Replace eslint space-infix-ops with equivalent typescript-eslint rule to support
+      // linting type definitions
+      'space-infix-ops': 'off',
+      '@typescript-eslint/space-infix-ops': [ 'error' ],
    },
 };
