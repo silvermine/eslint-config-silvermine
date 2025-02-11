dc210a00282fde7dba8913e2ed1586212a9d3d52
Matheus Baldi <matheushbaldi@gmail.com>
2025-02-11 16:29:52 -0300

    config: migrate array-bracket-spacing to stylistic

--- partials/base.js
+++ partials/base.js
@@ -120,7 +120,7 @@ module.exports = {
       'no-process-env': 'error',
       'no-process-exit': 'error',
       'no-sync': 'error',
-      'array-bracket-spacing': [ 'error', 'always' ],
+      '@stylistic/array-bracket-spacing': [ 'error', 'always' ],
       'block-spacing': 'error',
       'camelcase': 'error',
       'comma-spacing': 'error',
