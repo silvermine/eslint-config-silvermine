a3a5c87310c0c123a50c59f6cca7e726435e5c21
Matheus Baldi <matheushbaldi@gmail.com>
2025-02-11 16:31:47 -0300

    config: migrate block-spacing to stylistic

--- partials/base.js
+++ partials/base.js
@@ -119,7 +119,7 @@ module.exports = {
       'no-process-exit': 'error',
       'no-sync': 'error',
       'array-bracket-spacing': [ 'error', 'always' ],
-      'block-spacing': 'error',
+      '@stylistic/block-spacing': 'error',
       'camelcase': 'error',
       'comma-spacing': 'error',
       'comma-style': 'error',
