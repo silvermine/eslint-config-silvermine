c6c207bfac0b44e743b4b331ccb296827f28e7d8
Matheus Baldi <matheushbaldi@gmail.com>
2025-02-11 16:56:04 -0300

    config: migrate space-before-function-paren to stylistic

--- partials/base.js
+++ partials/base.js
@@ -204,7 +204,7 @@ module.exports = {
       'semi': [ 'error', 'always' ],
       '@stylistic/semi-spacing': 'error',
       '@stylistic/space-before-blocks': 'error',
-      'space-before-function-paren': [
+      '@stylistic/space-before-function-paren': [
          'error',
          {
             'anonymous': 'never',
