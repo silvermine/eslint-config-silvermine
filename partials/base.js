540738d4cdbbf24463b2fc980003c36bf8c379c5
Matheus Baldi <matheushbaldi@gmail.com>
2025-02-11 16:39:45 -0300

    config: migrate keyword-spacing to stylistic

--- partials/base.js
+++ partials/base.js
@@ -129,7 +129,7 @@ module.exports = {
       'consistent-this': [ 'error', 'self' ],
       '@stylistic/eol-last': 'error',
       '@stylistic/key-spacing': 'error',
-      'keyword-spacing': [
+      '@stylistic/keyword-spacing': [
          'error',
          {
             'overrides': {
