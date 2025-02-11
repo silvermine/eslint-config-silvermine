ff203728e9b66fccd8587f5cb31b020523c4b418
Matheus Baldi <matheushbaldi@gmail.com>
2025-02-11 16:46:12 -0300

    config: migrate padding-line-between-statements to stylistic

--- partials/base.js
+++ partials/base.js
@@ -158,7 +158,7 @@ module.exports = {
          { 'capIsNewExceptions': [ 'Q' ] },
       ],
       'new-parens': 'error',
-      'padding-line-between-statements': [
+      '@stylistic/padding-line-between-statements': [
          'error',
          { blankLine: 'always', prev: [ 'var', 'let', 'const' ], next: '*' },
       ],
